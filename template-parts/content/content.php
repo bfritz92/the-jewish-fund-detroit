ecab597de5654010ef260e034510eb0257533a78
bfritz92 <67482120+bfritz92@users.noreply.github.com>
2020-07-31 12:13:56 -0400

    color and padding

--- template-parts/content/content.php
+++ template-parts/content/content.php
@@ -12,7 +12,7 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header show limit-900">
+	<header class="entry-header show limit-900 dark-blue pt2">
 		<?php
 		if ( is_sticky() && is_home() && ! is_paged() ) {
 			printf( '<span class="sticky-post">%s</span>', _x( 'Featured', 'post', 'twentynineteen' ) );
