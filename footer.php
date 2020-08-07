4373d450ade3e99048f76da1bcffd87491646965
bfritz92 <67482120+bfritz92@users.noreply.github.com>
2020-08-07 10:28:24 -0400

    just the fade ins then

--- footer.php
+++ footer.php
@@ -104,31 +104,9 @@ window.addEventListener("scroll", event => {
 
 <!-- Fade-in script -->
 <script>
-const header = document.querySelector("masthead");
-const sectionOne = document.querySelector(".home-intro");
-
 const faders = document.querySelectorAll(".fade-in");
 const sliders = document.querySelectorAll(".slide-in");
 
-const sectionOneOptions = {
-  rootMargin: "-200px 0px 0px 0px"
-};
-
-const sectionOneObserver = new IntersectionObserver(function(
-  entries,
-  sectionOneObserver
-) {
-  entries.forEach(entry => {
-    if (!entry.isIntersecting) {
-      header.classList.add("nav-scrolled");
-    } else {
-      header.classList.remove("nav-scrolled");
-    }
-  });
-},
-sectionOneOptions);
-
-sectionOneObserver.observe(sectionOne);
 
 const appearOptions = {
   threshold: 0,
