593bc362bfeb108a9329138d510f2c52d685aea4
bfritz92 <67482120+bfritz92@users.noreply.github.com>
2020-08-07 10:05:22 -0400

    copy/paste

--- footer.php
+++ footer.php
@@ -104,33 +104,59 @@ window.addEventListener("scroll", event => {
 
 <!-- Fade-in script -->
 <script>
-const faders = document.querySelectorAll('.fade-in');
-const appearOptions = {
-	threshold: 1,
-	rootMargin: "0px 0px -100px 0px"
+const header = document.querySelector("header");
+const sectionOne = document.querySelector(".home-intro");
+
+const faders = document.querySelectorAll(".fade-in");
+const sliders = document.querySelectorAll(".slide-in");
+
+const sectionOneOptions = {
+  rootMargin: "-200px 0px 0px 0px"
 };
-const appearOnScroll = new IntersectionObserver
-	(function(
-		entries, 
-		appearOnScroll
+
+const sectionOneObserver = new IntersectionObserver(function(
+  entries,
+  sectionOneObserver
 ) {
-	// forcing changes
-	entries.forEach(entry => {
-		if (!entry.inIntersecting){
-			return;
-		} else {
-			entry.target.classList.add('appear');
-			appearOnScroll.unobserve(entry.target);
-		}
-	});
-}, 
-// forcing changes
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      header.classList.add("nav-scrolled");
+    } else {
+      header.classList.remove("nav-scrolled");
+    }
+  });
+},
+sectionOneOptions);
+
+sectionOneObserver.observe(sectionOne);
+
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -250px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function(
+  entries,
+  appearOnScroll
+) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+},
 appearOptions);
 
 faders.forEach(fader => {
-	appearOnScroll.observe(fader);
+  appearOnScroll.observe(fader);
 });
 
+sliders.forEach(slider => {
+  appearOnScroll.observe(slider);
+});
 </script>
 </body>
 </html>
