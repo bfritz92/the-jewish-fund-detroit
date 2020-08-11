aec6d5fb7d862cec082b217f29e0637840e60cf5
bfritz92 <67482120+bfritz92@users.noreply.github.com>
2020-08-11 11:43:28 -0400

    trying new JS to handle nav

--- footer.php
+++ footer.php
@@ -49,7 +49,7 @@
 <?php wp_footer(); ?>
 
 <!-- Menu subnavs -->       
-<script >
+<!-- <script >
 	//menu functions
 	//grabbing constants
 	const menuToggle = document.querySelectorAll('.menu-toggle');
@@ -71,6 +71,53 @@
  	current.querySelector("ul").classList.toggle("show");
 
 	}
+
+</script> -->
+<script>
+	const toggle = document.querySelector(".site-header");
+const menu = document.querySelector(".menu");
+const items = document.querySelectorAll(".menu-item");
+
+/* Toggle mobile menu */
+function toggleMenu() {
+  if (menu.classList.contains("show")) {
+    menu.classList.remove("show");
+    toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
+  } else {
+    menu.classList.add("showz");
+    toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
+  }
+}
+
+/* Activate Submenu */
+function toggleItem() {
+  if (this.classList.contains("submenu-active")) {
+    this.classList.remove("submenu-active");
+  } else if (menu.querySelector(".submenu-active")) {
+    menu.querySelector(".submenu-active").classList.remove("submenu-active");
+    this.classList.add("submenu-active");
+  } else {
+    this.classList.add("submenu-active");
+  }
+}
+
+/* Close Submenu From Anywhere */
+function closeSubmenu(e) {
+  let isClickInside = menu.contains(e.target);
+
+  if (!isClickInside && menu.querySelector(".submenu-active")) {
+    menu.querySelector(".submenu-active").classList.remove("submenu-active");
+  }
+}
+/* Event Listeners */
+toggle.addEventListener("click", toggleMenu, false);
+for (let item of items) {
+  if (item.querySelector(".submenu")) {
+    item.addEventListener("click", toggleItem, false);
+  }
+  item.addEventListener("keypress", toggleItem, false);
+}
+document.addEventListener("click", closeSubmenu, false);
 
 </script>
 
